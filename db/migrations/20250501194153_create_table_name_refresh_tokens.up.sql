52cc84980cf8f4f6e691bc3a84e532ea0c0f5144
Bangdams <sadamjr67@gmail.com>
2025-05-03 14:47:02 +0700

    menghapus field id dari table refresh_tokens

--- db/migrations/20250501194153_create_table_name_refresh_tokens.up.sql
+++ db/migrations/20250501194153_create_table_name_refresh_tokens.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE refresh_tokens (
-  id INT AUTO_INCREMENT,
   user_id INT NOT NULL,
   token TEXT NOT NULL,
+  status_logout TINYINT NOT NULL,
   expires_at TIMESTAMP NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
+  PRIMARY KEY (user_id),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
